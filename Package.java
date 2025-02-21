0572e253fbb40834ee2af83cedd49f3029386479
pbhuyar <131512700+pbhuyar@users.noreply.github.com>
2025-02-21 16:58:27 +0530

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,10 @@
+package a.b.c.d.e;
+public class Package 
+{
+		public int a ;
+		public void display()
+		{		
+			System.out.println("display method called");
+			System.out.println("a = "+a);
+		}
+}
---
+++ Relatedpackage.java
@@ -0,0 +1,12 @@
+import a.b.c.d.e.Package;
+class Relatedpackage 
+{
+	public static void main (String args [])
+	{
+		Package p  = new Package();
+		p.a= 10;
+		p.display();
+		Package pck = new Package();
+		pck.a=20;
+		pck.display();
+	}
